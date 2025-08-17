54edd133f07340601b73f17ac143a30ecde74126
Sawera Waheed <safanoorfsc24@gmail.com>
2025-08-17 11:01:48 -0700

    added consoles

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -3,12 +3,13 @@ export function isNowInTimeWindow(window: string): boolean {
     const now = new Date();
     const start = new Date(now);
     const end = new Date(now);
-
+console.log({now,start,end})
     const [startH, startM] = startStr.split(":").map(Number);
     const [endH, endM] = endStr.split(":").map(Number);
 
     start.setHours(startH, startM, 0, 0);
     end.setHours(endH, endM, 0, 0);
-
+    console.log("after the calculation")
+console.log({start,end})
     return now >= start && now <= end;
 }
