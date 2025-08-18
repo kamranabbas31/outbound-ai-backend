9cb742268e8c57f9faed77cf20d05840e3be021a
Sawera Waheed <safanoorfsc24@gmail.com>
2025-08-18 05:20:50 -0700

    Despositions fixed

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -1,4 +1,12 @@
-import { RedisOptions } from 'ioredis';
+import { Redis, RedisOptions } from 'ioredis';
+import { ConnectionOptions } from 'tls';
+export const redis = new Redis({
+  host: process.env.REDIS_HOST,
+  port: 6379,
+  password: process.env.REDIS_PASSWORD,
+  tls: { rejectUnauthorized: false } as ConnectionOptions,
+  maxRetriesPerRequest: null,
+});
 
 export const redisConfig: RedisOptions = {
   host: process.env.REDIS_HOST,
