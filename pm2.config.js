4d31dd2b4d6c6a0c8fff975c5887d89b69111d72
Sawera Waheed <safanoorfsc24@gmail.com>
2025-08-19 03:58:05 -0700

    pm2 config modified

--- pm2.config.js
+++ pm2.config.js
@@ -8,7 +8,7 @@ module.exports = {
       },
     },
     {
-      name: 'cadence-worker',
+      name: 'worker',
       script: 'dist/worker/cadence.worker.js',
       env: {
         NODE_TLS_REJECT_UNAUTHORIZED: '0',
